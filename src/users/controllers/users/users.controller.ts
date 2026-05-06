6ac5a023fc6c12641592df1e456e755f9776ac57
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-06 13:12:29 +0600

    feat(users): add skeleton methods to controller and service

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    getUsers(){
+
+    }
+
+
+    @Post()
+    createUser(){
+        
+    }
+}
